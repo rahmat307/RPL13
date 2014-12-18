61073046257e8c875e7054dec8cea9666d4ff81d
Rofi Miqdad <Rofi Miqdad@Rofi.mshome.net>
2014-12-18 16:57:40 +0700

    Integrasi GIT di Eclipse

--- src/rpl.java
+++ src/rpl.java
@@ -4,6 +4,7 @@ public class rpl {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Tugas RPL1 13");
+		System.out.println("Integrasi GIT ke Eclipse");
 	}
 
 }
